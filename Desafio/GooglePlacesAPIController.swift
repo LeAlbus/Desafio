51540a9cfedf2f588bce0a2166ef3ea7d797bb37
LeAlbus <jpedro_lopes21@hotmail.com>
2016-11-19 19:38:21 -0200

    Google Maps/Google PLaces frameworks added

---
+++ Desafio/GooglePlacesAPIController.swift
@@ -0,0 +1,10 @@
+//
+//  GooglePlacesAPIController.swift
+//  Desafio
+//
+//  Created by Pedro Lopes on 11/19/16.
+//  Copyright © 2016 Pedro Lopes. All rights reserved.
+//
+
+import Foundation
+import GoogleMaps
---
+++ Desafio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Desafio
+//
+//  Created by Pedro Lopes on 11/19/16.
+//  Copyright © 2016 Pedro Lopes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
